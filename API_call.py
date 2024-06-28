3427bc344eea4a83a473ff974e38fc99cd42b41f
marmot8080 <marmot8080@gmail.com>
2024-06-28 09:09:36 +0900

    implemented indirect input for API key

--- API_call.py
+++ API_call.py
@@ -1,4 +1,8 @@
 import API_manager
 
 if __name__ == '__main__':
-    api_manager = API_manager.api_manager("")
+    f = open("API_key.txt", 'r')
+    api_key = f.readline()
+    f.close()
+    
+    api_manager = API_manager.api_manager(api_key)
